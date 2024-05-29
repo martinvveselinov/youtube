2dce68ad41293055ade32bb6cfec23039dbadb88
Martin Veselinov <martinvveselinov@gmail.com>
2024-05-29 22:32:54 +0300

    Commit

---
+++ src/components/Videos.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import { Stack, Box, Typography, Button } from "@mui/material";
+
+import { ChannelCard, Loader, VideoCard } from "./";
+
+const Videos = ({ videos, direction, addToPlaylist, removeFromPlaylist, playlist }) => {
+  if (!videos?.length) return <Loader />;
+
+    return (
+      <Stack direction={direction || "row"} flexWrap="wrap" justifyContent="start" alignItems="start" gap={2}>
+        {videos.map((item, idx) => (
+          <Box key={idx}>
+            {item.id && <VideoCard video={item} />}
+            {item.id.channelId && <ChannelCard channelDetail={item} />}
+            {item.statistics.likeCount && <Typography
+            variant="span"
+            fontWeight="bold"
+            mb={2}
+            sx={{ color: "white" }}
+          >
+            Likes: <span style={{ color: "#FC1503" }}>{item.statistics.likeCount}</span>
+          </Typography>} 
+          {item.statistics.likeCount && <Typography
+            variant="span"
+            fontWeight="bold"
+            mb={2}
+            sx={{ color: "white" }}
+          >
+            <br></br>Comments: <span style={{ color: "#FC1503" }}>{item.statistics.commentCount}</span> <br></br>
+          </Typography>}
+          
+          {playlist.includes(item.id.videoId ?? item.id) ? (
+            <Button variant="contained" color="secondary" onClick={() => removeFromPlaylist(item.id.videoId ?? item.id)}>
+              Remove from Playlist
+            </Button>
+          ) : (
+            <Button variant="contained" color="primary" onClick={() => addToPlaylist(item)}>
+              Add to Playlist
+            </Button>
+          )}
+          </Box>
+        ))}
+      </Stack>
+    );
+}
+
+
+export default Videos;
